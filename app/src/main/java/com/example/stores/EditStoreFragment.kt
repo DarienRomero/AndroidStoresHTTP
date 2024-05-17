bc1addbf5d7306c5c9d43076da6d96cc51648885
Darien Romero <darien.r.leiva@gmail.com>
2024-05-17 15:23:17 -0500

    avances

--- app/src/main/java/com/example/stores/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores/EditStoreFragment.kt
@@ -3,6 +3,9 @@ package com.example.stores
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import com.example.stores.databinding.FragmentEditStoreBinding
@@ -10,6 +13,7 @@ import com.example.stores.databinding.FragmentEditStoreBinding
 class EditStoreFragment : Fragment() {
 
     private lateinit var mBinding: FragmentEditStoreBinding
+    private var mActivity: MainActivity? = null
 
 
     override fun onCreateView(
@@ -19,6 +23,32 @@ class EditStoreFragment : Fragment() {
         mBinding = FragmentEditStoreBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return mBinding.root
+    }
+    //Ocurre después del onCreateView
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mActivity = activity as? MainActivity
+        mActivity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mActivity?.supportActionBar?.title = getString(R.string.edit_store_title_add)
+        setHasOptionsMenu(true)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save, menu)
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home -> {
+                mActivity?.onBackPressedDispatcher?.onBackPressed()
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
